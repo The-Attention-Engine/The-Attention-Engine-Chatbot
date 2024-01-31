ce15ff1389f35c9b1a195508af27320205d0f39e
ashishb012 <ashishb0128@gmail.com>
2024-01-31 19:20:11 +0530

    replicate-image-api

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -1,51 +1,88 @@
+import Replicate from "replicate";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import { Configuration, OpenAIApi } from "openai-edge";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN || "",
 });
-
-const openai = new OpenAIApi(configuration);
-
-export const runtime = "edge";
 
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { prompt, amount = 1, resolution = "512x512" } = body;
+    const { prompt } = body;
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    if (!configuration.apiKey) {
-      return new NextResponse("OpenAI API Key not configured.", {
-        status: 500,
-      });
     }
 
     if (!prompt) {
       return new NextResponse("Prompt is required", { status: 400 });
     }
 
-    if (!amount) {
-      return new NextResponse("Amount is required", { status: 400 });
-    }
+    const response = await replicate.run(
+      "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
+      {
+        input: {
+          prompt,
+        },
+      }
+    );
 
-    if (!resolution) {
-      return new NextResponse("Resolution is required", { status: 400 });
-    }
-
-    const response = await openai.createImage({
-      prompt,
-      n: parseInt(amount, 10),
-      size: resolution,
-    });
-
-    return NextResponse.json(response.url);
+    return NextResponse.json(response);
   } catch (error) {
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
+
+// import { auth } from "@clerk/nextjs";
+// import { NextResponse } from "next/server";
+// import { Configuration, OpenAIApi } from "openai-edge";
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+// export const runtime = "edge";
+
+// export async function POST(req: Request) {
+//   try {
+//     const { userId } = auth();
+//     const body = await req.json();
+//     const { prompt, amount = 1, resolution = "512x512" } = body;
+
+//     if (!userId) {
+//       return new NextResponse("Unauthorized", { status: 401 });
+//     }
+
+//     if (!configuration.apiKey) {
+//       return new NextResponse("OpenAI API Key not configured.", {
+//         status: 500,
+//       });
+//     }
+
+//     if (!prompt) {
+//       return new NextResponse("Prompt is required", { status: 400 });
+//     }
+
+//     if (!amount) {
+//       return new NextResponse("Amount is required", { status: 400 });
+//     }
+
+//     if (!resolution) {
+//       return new NextResponse("Resolution is required", { status: 400 });
+//     }
+
+//     const response = await openai.createImage({
+//       prompt,
+//       n: parseInt(amount, 10),
+//       size: resolution,
+//     });
+
+//     return NextResponse.json(response.url);
+//   } catch (error) {
+//     return new NextResponse("Internal Error", { status: 500 });
+//   }
+// }
