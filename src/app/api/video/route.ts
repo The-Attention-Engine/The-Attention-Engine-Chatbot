c2cb416383218df1b60c20ca54a2a54cbf1d8073
ashishb012 <ashishb0128@gmail.com>
2024-01-31 19:15:53 +0530

    video-ui-api

--- src/app/api/video/route.ts
+++ src/app/api/video/route.ts
@@ -3,7 +3,7 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN!,
+  auth: process.env.REPLICATE_API_TOKEN || "",
 });
 
 export async function POST(req: Request) {
@@ -21,10 +21,10 @@ export async function POST(req: Request) {
     }
 
     const response = await replicate.run(
-      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      "anotherjesse/zeroscope-v2-xl:71996d331e8ede8ef7bd76eba9fae076d31792e4ddf4ad057779b443d6aea62f",
       {
         input: {
-          prompt_a: prompt,
+          prompt,
         },
       }
     );
